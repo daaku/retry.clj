6485e95429e3357f457c09c58b45786148675643
Naitik Shah <n@daaku.org>
2011-11-13 21:55:42 +0000

    project metadata

--- project.clj
+++ project.clj
@@ -1,5 +1,7 @@
 (defproject retry "1.0.2"
   :description "Retry on some exceptions."
+  :author "Naitik Shah <n@daaku.org>"
+  :url "https://github.com/nshah/retry.clj"
   :dependencies
     [[org.clojure/clojure "1.3.0"]
      [slingshot "0.8.0"]]
