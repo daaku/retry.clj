8a27270c28a64ea3e1756f3beb04753330d962c5
Naitik Shah <n@daaku.org>
2012-03-02 15:48:08 -0800

    remove dev-dependencies and repl-init

--- project.clj
+++ project.clj
@@ -2,14 +2,6 @@
   :description "Retry on some exceptions."
   :author "Naitik Shah <n@daaku.org>"
   :url "https://github.com/nshah/retry.clj"
-  :repl-init retry.repl
-  :checksum-deps true
-  :exclusions [org.clojure/clojure]
   :dependencies
     [[org.clojure/clojure "1.3.0"]
-     [slingshot "0.10.1"]]
-  :dev-dependencies
-    [[auto-reload "1.0.3"]
-     [lein-marginalia "0.7.0-20111019.122151-1"]
-     [org.clojure/tools.logging "0.2.3"]
-     [vimclojure/server "2.3.1"]])
+     [slingshot "0.10.1"]])
